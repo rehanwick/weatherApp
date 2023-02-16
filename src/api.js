b583ea3c9e5c13a6a4f373db98e73a0463cdc75d
rehanWick <rs0327884@gmail.com>
2023-02-16 10:57:14 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "bbb3e7422a167706e48d1a82dc894a62"; 
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "327e4b3ea8msh2090dbb68fea318p146c9djsn838598697acd",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+
+// https://wft-geo-db.p.rapidapi.com/v1/geo/cities
